2a9083c3d16f8abccaeb61e36227e14a449dc3e5
Andrii Nemshylov <nemshylovandrii1290@gmail.com>
2026-02-09 13:58:00 +0100

    fix-landing-page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const form = document.getElementById('contactForm');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // window.alert('Форма успішно відправлена (без перезавантаження сторінки)');
+
+  form.reset();
+});
