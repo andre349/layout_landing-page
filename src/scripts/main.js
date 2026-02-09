ee429b8e6d2dd2db9648dbc87e46471ddb58b075
Andrii Nemshylov <nemshylovandrii1290@gmail.com>
2026-02-09 14:20:27 +0100

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,1 @@
 'use strict';
-
-const form = document.getElementById('contactForm');
-
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  // window.alert('Форма успішно відправлена (без перезавантаження сторінки)');
-
-  form.reset();
-});
